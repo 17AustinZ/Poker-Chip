61eb870c18087363f289c51b9dcd8d38f6a679bf
Austin Zhang <azhang042@gmail.com>
2015-07-20 10:51:56 -0700

    Switched bettingStructure to enums

--- ViewControllers/ConfigureViewController.swift
+++ ViewControllers/ConfigureViewController.swift
@@ -19,6 +19,15 @@ import UIKit
 
 class ConfigureViewController: UIViewController{
     
+    var betStructure = bettingStructure.NoLimit
+
+    enum bettingStructure {
+        case NoLimit
+        case FixedLimit
+        case PotLimit
+        case SpreadLimit
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.setNavigationBarHidden(true, animated: true)
@@ -30,20 +39,41 @@ class ConfigureViewController: UIViewController{
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
+    @IBOutlet weak var bettingStructureDisplay: UILabel!
 
     @IBAction func changeBettingStructure(sender: AnyObject) {
         let CBSAlert = SimpleAlert.Controller(title: "Betting Structure", message: "", style: .Alert)
 
-        CBSAlert.addAction(SimpleAlert.Action(title: "No Limit", style: .Default , handler: {(alertAction : SimpleAlert.Action!) -> Void in
-            println("done")
-            }))
+        
+        CBSAlert.addAction(SimpleAlert.Action(title: "No Limit", style: SimpleAlert.Action.Style.Default , handler: {(alertAction : SimpleAlert.Action!) -> Void in
+            self.betStructure = .NoLimit
+        }))
         
         CBSAlert.addAction(SimpleAlert.Action(title: "Fixed Limit", style: .Default , handler: {(alertAction : SimpleAlert.Action!) -> Void in
-            println("done")
+            self.betStructure = .FixedLimit
         }))
-
-
+        
+        CBSAlert.addAction(SimpleAlert.Action(title: "Pot Limit", style: .Default , handler: {(alertAction : SimpleAlert.Action!) -> Void in
+            self.betStructure = .PotLimit
+        }))
+        
+        CBSAlert.addAction(SimpleAlert.Action(title: "Spread Limit", style: .Default , handler: {(alertAction : SimpleAlert.Action!) -> Void in
+            self.betStructure = .SpreadLimit
+        }))
+        switch betStructure{
+        case .FixedLimit:
+            self.bettingStructureDisplay.text = "Fixed Limit"
+        case .NoLimit:
+            self.bettingStructureDisplay.text = "No Limit"
+        case .PotLimit:
+            self.bettingStructureDisplay.text = "Pot Limit"
+        case .SpreadLimit:
+            self.bettingStructureDisplay.text = "Spread Limit"
+        }
+        
         presentViewController(CBSAlert, animated: true, completion: nil)
     }
+    
+    
     
 }
