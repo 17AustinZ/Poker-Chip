9bce91947fb11072944a1f6195f49c760c3dfa22
Austin Zhang <azhang042@gmail.com>
2015-08-05 11:23:09 -0700

    Added selection for betting structure option table view

--- ViewControllers/BettingStructureOptionViewController.swift
+++ ViewControllers/BettingStructureOptionViewController.swift
@@ -11,20 +11,28 @@ import Foundation
 class BettingStructureOptionViewController: UITableViewController{
     var ConfigVC : ConfigureTableViewController?
     
-//    override func viewDidLoad() {
-//        
-//    }
+    //    override func viewDidLoad() {
+    //
+    //    }
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         println("selected")
         println(indexPath)
         println(indexPath.indexAtPosition(1))
+        
+        var oldPath = ConfigVC?.selectedStructurePath
+        if oldPath != nil{
+            tableView.cellForRowAtIndexPath(oldPath!)!.accessoryType = UITableViewCellAccessoryType.None
+            
+        }
         var index : Int = indexPath.indexAtPosition(1)
         ConfigVC?.selectedStructurePath = indexPath
         
+        
+        tableView.cellForRowAtIndexPath(indexPath)!.accessoryType = UITableViewCellAccessoryType.Checkmark
         tableView.cellForRowAtIndexPath(indexPath)?.detailTextLabel?.text = "asdf"
-//        tableView.cellForRowAtIndexPath(indexPath)?.setNeedsLayout()
-//        tableView.cellForRowAtIndexPath(indexPath)?.setNeedsDisplay()
+        //        tableView.cellForRowAtIndexPath(indexPath)?.setNeedsLayout()
+        //        tableView.cellForRowAtIndexPath(indexPath)?.setNeedsDisplay()
     }
     
-
+    
 }
